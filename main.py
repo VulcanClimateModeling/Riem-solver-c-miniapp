e3d3fa02145285ddbecfcc3ba168986cc7c9875a
Johann Dahm <johannd@vulcan.com>
2020-11-21 15:41:29 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import sys
+from collections.abc import Iterable
+
+import gt4py
+import gt4py.storage as gt_storage
+import netCDF4 as nc
+import numpy as np
+from gt4py.gtscript import I, J, K, IJ, IJK, Field, stencil
+
+from riem_solver_c import riem_solver_c
+
+def iterable(obj):
+    return isinstance(obj, Iterable)
+
+class Dataset:
+    def __init__(self, file_name, backend):
+        self._dataset = nc.Dataset(file_name)
+        self._backend = backend
+        self.npx = int(self._dataset["npx"][0].item())
+        self.npy = int(self._dataset["npy"][0].item())
+        self.ng = int(self._dataset["ng"][0].item())
+        self.km = int(self._dataset["km"][0].item())
+
+    @staticmethod
+    def _find_fuzzy(axes, name):
+        return next(i for i, axis in enumerate(axes) if axis.startswith(name))
+
+    def netcdf_to_gt4py(self, var):
+        """Convert a netcdf variable to gt4py storage."""
+        axes = [d.name for d in var.get_dims()]
+        idim = self._find_fuzzy(axes, "xaxis")
+        jdim = self._find_fuzzy(axes, "yaxis")
+        kdim = self._find_fuzzy(axes, "zaxis")
+        if np.prod(var.shape) > 1:
+            permutation = [dim for dim in (idim, jdim, kdim) if dim]
+            # put other axes at the back
+            for i in range(len(axes)):
+                if i not in permutation:
+                    permutation.append(i)
+            ndarray = np.squeeze(np.transpose(var, permutation))
+            if len(ndarray.shape) == 3:
+                origin = (self.ng, self.ng, 0)
+            elif len(ndarray.shape) == 2:
+                origin = (self.ng, self.ng)
+            else:
+                origin = (0,)
+            return gt_storage.from_array(
+                ndarray, GT4PY_BACKEND, default_origin=origin, shape=ndarray.shape)
+        else:
+            return var[0].item()
+
+    def __getitem__(self, index):
+        variable = self._dataset[index]
+        return self.netcdf_to_gt4py(variable)
+
+    def new(self, axes, dtype, pad_k=False):
+        k_add = 1 if pad_k else 0
+        if axes == IJK:
+            origin = (self.ng, self.ng, 0)
+            shape = (2 * self.ng + self.npx, 2 * self.ng + self.npy, self.km + k_add)
+            mask = None
+        elif axes == IJ:
+            mask = (True, True, False)
+            origin = (self.ng, self.ng)
+            shape = (2 * self.ng + self.npx, 2 * self.ng + self.npy)
+        elif axes == K:
+            mask = (False, False, True)
+            origin = (0,)
+            shape = (self.km + k_add, )
+        else:
+            raise ValueError("Axes unrecognized")
+        return gt_storage.empty(backend=self._backend, default_origin=origin, shape=shape, dtype=dtype, mask=mask)
+
+
+def do_test(data_file, backend):
+    data = Dataset(data_file, backend)
+
+    # other fields
+    pe = data.new(IJK, float, pad_k=True)
+
+    riem = stencil(backend=backend, definition=riem_solver_c, externals={"A_IMP": data["a_imp"]})
+
+    riem(data["ms"],
+          data["dt"],
+          data["akap"],
+          data["cappa"],
+          data["cp"],
+          data["ptop"],
+          data["hs"],
+          data["w3"],
+          data["pt"],
+          data["q_con"],
+          data["delp"],
+          data["gz"],
+          data["pef"],
+          data["ws"],
+          data["p_fac"],
+          data["scale_m"],
+          pe)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: main.py path/to/dataset.nc [backend]")
+    file_name = sys.argv[1]
+    if len(sys.argv) > 2:
+        backend = sys.argv[2]
+    else:
+        backend = "numpy"
+    do_test(file_name, backend)
---
+++ riem_solver_c.py
@@ -0,0 +1,215 @@
+from gt4py.gtscript import (__INLINED, BACKWARD, FORWARD, IJ, IJK, PARALLEL,
+                            Field, I, J, K, computation, exp, interval, log)
+
+MOIST_CAPPA: bool = True
+USE_COND: bool = True
+GRAV: float = 9.8
+RDGAS: float = 287.04
+
+
+def calc_t1g(dt, gama):
+    if __INLINED(MOIST_CAPPA):
+        t1g = 2. * dt * dt
+    else:
+        t1g = gama * 2. * dt * dt
+    return t1g
+
+
+def calc_dz2(dm2, pt, cp2, akap, p_fac, pm2, p1):
+    if __INLINED(MOIST_CAPPA):
+        fac = cp2 - 1.
+    else:
+        fac = akap - 1
+    return -dm2 * RDGAS * pt * exp(fac * log(max(p_fac * pm2, p1 + pm2)))
+
+
+def riem_solver_c(
+        ms: int, dt: float, akap: float, cappa: Field[float, IJK],
+        cp: float, ptop: float, hs: Field[float, IJ],
+        w3: Field[float, IJK],
+        pt: Field[float, IJK],
+        q_con: Field[float, IJK],
+        delp: Field[float, IJK],
+        gz: Field[float, IJK],
+        pef: Field[float, IJK],
+        ws: Field[float, IJK],
+        p_fac: float, scale_m: float, pe: Field[float, IJK]):
+    """
+    C-grid Riemann solver.
+
+    Args:
+        ms: ...
+        dt: Timestep
+        akap: TBD
+        cappa: TBD
+        cp: TBD
+        ptop: TBD
+        hs: TBD
+        w3: TBD
+        pt: TBD
+        q_con: TBD
+        delp: TBD
+        gz: TBD [inout]
+        pef: TBD [out]
+        ws: TBD
+        p_fac: TBD
+        scale_m: TBD
+    """
+    from __externals__ import A_IMP
+
+    with computation(PARALLEL), interval(...):
+        dm = delp
+        gama = 1. / (1. - akap)
+    with computation(FORWARD):
+        with interval(0, 1):
+            pef = ptop
+            pem = ptop
+            if __INLINED(USE_COND):
+                peg = ptop
+
+        with interval(2, None):
+            pem = pem[0, 0, -1] + dm[0, 0, -1]
+            if __INLINED(USE_COND):
+                peg = peg[0, 0, -1] + dm[0, 0, -1] * (1. - q_con[0, 0, -1])
+
+    with computation(PARALLEL), interval(0, -1):
+        dz2 = gz[0, 0, 1] - gz[0, 0, 0]
+
+        if __INLINED(USE_COND):
+            pm2 = (peg[0, 0, 1] - peg[0, 0, 0]) / log(peg[0, 0, 1] / peg[0, 0, 0])
+            if __INLINED(MOIST_CAPPA):
+                cp2 = cappa
+                gm2 = 1. / (1. - cp2)
+        else:
+            pm2 = dm / log(pem[0, 0, 1] / pem[0, 0, 0])
+
+        dm = dm * 1. / GRAV
+        w2 = w3
+
+    # sim1_solver {
+    # should only enter this if a_imp > 0.5, but a_imp = 1 in the test code
+    # we are addressing this limitation...
+    # Line 232
+    with computation(PARALLEL), interval(0, -1):
+        if __INLINED(A_IMP < 0.5):
+            if __INLINED(MOIST_CAPPA):
+                fac = gm2
+            else:
+                fac = gama
+            pe = exp(fac * log(-dm / dz2 * RDGAS * pt)) - pm2
+
+            w1 = w2
+
+    # Line 245
+    with computation(PARALLEL), interval(1, -2):
+        if __INLINED(A_IMP < 0.5):
+            g_rat = dm[0, 0, 0] / dm[0, 0, 1]
+            bb = 2. * (1. + g_rat)
+            dd = 3. * (pe + g_rat) * pe[0, 0, 1]
+        # with interval(-2, -1):
+        #     bb = 2.0
+        #     dd = 3.0 * pe
+
+    # Line 255
+    with computation(PARALLEL):
+        # Set special conditions on pp, bb, dd
+        with interval(0, 1):
+            if __INLINED(A_IMP < 0.5):
+                pp = 0
+        with interval(1, 2):
+            if __INLINED(A_IMP < 0.5):
+                pp = dd[0, 0, -1] / bb[0, 0, -1]
+        with interval(-2, -1):
+            if __INLINED(A_IMP < 0.5):
+                bb = 2.
+                dd = 3 * pe
+
+    with computation(FORWARD), interval(2, 1):
+        if __INLINED(A_IMP < 0.5):
+            bet = bb[0, 0, -1]
+
+    # Line 265
+    with computation(PARALLEL), interval(2, -1):
+        if __INLINED(A_IMP < 0.5):
+            gam = g_rat[0, 0, -1] / bet[0, 0, 0]
+            bet = bb - gam
+
+    with computation(PARALLEL), interval(3, None):
+        if __INLINED(A_IMP < 0.5):
+            pp = (dd[0, 0, 0] - pp[0, 0, 0]) / bet[0, 0, -1]
+
+    # Line 275
+    with computation(BACKWARD), interval(1, -1):  # this may need to be -2
+        if __INLINED(A_IMP < 0.5):
+            pp = pp - gam * pp[0, 0, 1]
+
+            aa = (pem[0, 0, 0] + pp[0, 0, 0]) / (dz2[0, 0, -1] + dz2[0, 0, 0])
+
+            t1g = calc_t1g(dt, gama)
+            if __INLINED(MOIST_CAPPA):
+                aa *= t1g * 0.5 * (gm2[0, 0, -1] + gm2[0, 0, 0])
+            else:
+                aa *= t1g
+
+    # Line 295
+    with computation(PARALLEL), interval(1, 2):
+        if __INLINED(A_IMP < 0.5):
+            bet = dm[0, 0, 0] - aa[0, 0, 1]
+            w2 = (dm[0, 0, 0] * w1[0, 0, 0] + dt * pp[0, 0, 1]) / bet
+
+    # Line 302
+    with computation(FORWARD), interval(2, -2):
+        if __INLINED(A_IMP < 0.5):
+            gam = aa / bet
+            bet = dm - (aa * (gam + 1) + aa[0, 0, 1])
+            w2 = (dm * w1 + dt * (pp[0, 0, 1] - pp[0, 0, 0]) - aa * w2[0, 0, -1]) / bet
+
+    # Line 312
+    with computation(FORWARD), interval(-2, -1):
+        if __INLINED(A_IMP < 0.5):
+            t1g = calc_t1g(dt, gam)
+            if __INLINED(MOIST_CAPPA):
+                p1 = t1g * gm2 / dz2 * (pem[0, 0, 1] + pp[0, 0, 1])
+            else:
+                p1 = t1g / dz2 * (pem[0, 0, 1] + pp[0, 0, 1])
+            gam = aa / bet
+            bet = dm - (aa * (1 + gam) + p1)
+            w2 = (dm * w1 + dt * (pp[0, 0, 1] - pp[0, 0, 0]
+                                  ) - p1 * ws - aa * w2[0, 0, -1]) / bet
+
+    # Line 325
+    with computation(BACKWARD), interval(0, -2):
+        if __INLINED(A_IMP < 0.5):
+            w2 -= gam[0, 0, 1] * w2[0, 0, 1]
+
+    # Line 338
+    with computation(FORWARD):
+        with interval(0, 1):
+            if __INLINED(A_IMP < 0.5):
+                pe = 0.
+        with interval(2, -1):
+            if __INLINED(A_IMP < 0.5):
+                rdt = 1. / dt
+                pe = pe[0, 0, -1] + dm * (w2 - w1) * rdt
+
+    # Line 346
+    with computation(BACKWARD):
+        with interval(-2, -1):
+            if __INLINED(A_IMP < 0.5):
+                r3 = 1. / 3.
+                p1 = (pe + 2. * pe[0, 0, 1]) * r3
+                dz2 = calc_dz2(dm, pt, cp2, akap, p_fac, pm2, p1)
+        with interval(0, -2):
+            if __INLINED(A_IMP < 0.5):
+                r3 = 1. / 3.
+                p1 = (pe + bb * pe[0, 0, 1] + g_rat * pe[0, 0, 2]) * r3 - g_rat * p1
+                dz2 = calc_dz2(dm, pt, cp2, akap, p_fac, pm2, p1)
+
+    # } SIM1_solver
+
+    # Line 177
+    with computation(PARALLEL), interval(2, None):
+        pef = pe + pem
+
+    with computation(BACKWARD), interval(1, -2):
+        gz = gz[0, 0, 1] - dz2 * GRAV
