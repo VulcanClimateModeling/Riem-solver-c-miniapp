11bc421f21ce94d44c1a07f6720167e12635aca8
Johann Dahm <johannd@vulcan.com>
2020-12-08 11:25:34 -0800

    Catch a few errors, update gt4py

--- riem_solver_c.py
+++ riem_solver_c.py
@@ -1,10 +1,14 @@
 from gt4py.gtscript import (__INLINED, BACKWARD, FORWARD, IJ, IJK, PARALLEL,
                             Field, I, J, K, computation, exp, interval, log)
 
-MOIST_CAPPA: bool = True
-USE_COND: bool = True
+MOIST_CAPPA: bool = False
+USE_COND: bool = False
 GRAV: float = 9.8
 RDGAS: float = 287.04
+
+
+def calc_gama(akap):
+    return 1. / (1. - akap)
 
 
 def calc_t1g(dt, gama):
@@ -24,16 +28,11 @@ def calc_dz2(dm2, pt, cp2, akap, p_fac, pm2, p1):
 
 
 def riem_solver_c(
-        ms: int, dt: float, akap: float, cappa: Field[float, IJK],
-        cp: float, ptop: float, hs: Field[float, IJ],
-        w3: Field[float, IJK],
-        pt: Field[float, IJK],
-        q_con: Field[float, IJK],
-        delp: Field[float, IJK],
-        gz: Field[float, IJK],
-        pef: Field[float, IJK],
-        ws: Field[float, IJK],
-        p_fac: float, scale_m: float, pe: Field[float, IJK]):
+        cappa: Field[float, IJK], hs: Field[float, IJ], w3: Field[float, IJK],
+        pt: Field[float, IJK], q_con: Field[float, IJK], delp: Field[float, IJK],
+        gz: Field[float, IJK], pef: Field[float, IJK], ws: Field[float, IJK],
+        pe: Field[float, IJK], p_fac: float, scale_m: float, ms: int, dt: float,
+        akap: float, cp: float, ptop: float):
     """
     C-grid Riemann solver.
 
@@ -57,9 +56,11 @@ def riem_solver_c(
     """
     from __externals__ import A_IMP
 
-    with computation(PARALLEL), interval(...):
+    # Line 121
+    with computation(PARALLEL), interval(0, -1):
         dm = delp
-        gama = 1. / (1. - akap)
+
+    # Line 129
     with computation(FORWARD):
         with interval(0, 1):
             pef = ptop
@@ -67,11 +68,12 @@ def riem_solver_c(
             if __INLINED(USE_COND):
                 peg = ptop
 
-        with interval(2, None):
+        with interval(1, None):
             pem = pem[0, 0, -1] + dm[0, 0, -1]
             if __INLINED(USE_COND):
                 peg = peg[0, 0, -1] + dm[0, 0, -1] * (1. - q_con[0, 0, -1])
 
+    # Line 151
     with computation(PARALLEL), interval(0, -1):
         dz2 = gz[0, 0, 1] - gz[0, 0, 0]
 
@@ -93,26 +95,24 @@ def riem_solver_c(
             if __INLINED(MOIST_CAPPA):
                 fac = gm2
             else:
-                fac = gama
+                fac = calc_gama(akap)
             pe = exp(fac * log(-dm / dz2 * RDGAS * pt)) - pm2
 
             w1 = w2
 
     # Line 245
-    with computation(PARALLEL), interval(1, -2):
+    with computation(PARALLEL), interval(0, -2):
         if __INLINED(A_IMP > 0.5):
             g_rat = dm[0, 0, 0] / dm[0, 0, 1]
             bb = 2. * (1. + g_rat)
             dd = 3. * (pe + g_rat) * pe[0, 0, 1]
-        # with interval(-2, -1):
-        #     bb = 2.0
-        #     dd = 3.0 * pe
 
     # Line 255
     with computation(FORWARD):
         # Set special conditions on pp, bb, dd
         with interval(0, 1):
             if __INLINED(A_IMP > 0.5):
+                bet = bb
                 pp = 0
         with interval(1, 2):
             if __INLINED(A_IMP > 0.5):
@@ -122,41 +122,37 @@ def riem_solver_c(
                 bb = 2.
                 dd = 3 * pe
 
-    with computation(FORWARD), interval(2, 3):
-        if __INLINED(A_IMP > 0.5):
-            bet = bb[0, 0, -1]
-
     # Line 265
-    with computation(PARALLEL), interval(2, -1):
+    with computation(PARALLEL), interval(1, -1):
         if __INLINED(A_IMP > 0.5):
-            gam = g_rat[0, 0, -1] / bet[0, 0, 0]
+            gam = g_rat[0, 0, -1] / bet[0, 0]
             bet = bb - gam
 
-    with computation(PARALLEL), interval(3, None):
+    with computation(PARALLEL), interval(2, None):
         if __INLINED(A_IMP > 0.5):
-            pp = (dd[0, 0, 0] - pp[0, 0, 0]) / bet[0, 0, -1]
+            pp = (dd[0, 0, -1] - pp[0, 0, -1]) / bet[0, 0]
 
-    # Line 275
+    # Line 275 (fused with 284)
     with computation(BACKWARD), interval(1, -1):  # this may need to be -2
         if __INLINED(A_IMP > 0.5):
             pp = pp - gam * pp[0, 0, 1]
 
-            aa = (pem[0, 0, 0] + pp[0, 0, 0]) / (dz2[0, 0, -1] + dz2[0, 0, 0])
-
+            gama = calc_gama(akap)
             t1g = calc_t1g(dt, gama)
+
+            aa = t1g * (pem[0, 0, 0] + pp[0, 0, 0]) / (dz2[0, 0, -1] + dz2[0, 0, 0])
+
             if __INLINED(MOIST_CAPPA):
-                aa *= t1g * 0.5 * (gm2[0, 0, -1] + gm2[0, 0, 0])
-            else:
-                aa *= t1g
+                aa *= 0.5 * (gm2[0, 0, -1] + gm2[0, 0, 0])
 
     # Line 295
-    with computation(PARALLEL), interval(1, 2):
+    with computation(PARALLEL), interval(0, 1):
         if __INLINED(A_IMP > 0.5):
             bet = dm[0, 0, 0] - aa[0, 0, 1]
             w2 = (dm[0, 0, 0] * w1[0, 0, 0] + dt * pp[0, 0, 1]) / bet
 
     # Line 302
-    with computation(FORWARD), interval(2, -2):
+    with computation(FORWARD), interval(1, -2):
         if __INLINED(A_IMP > 0.5):
             gam = aa / bet
             bet = dm - (aa * (gam + 1) + aa[0, 0, 1])
@@ -166,12 +162,11 @@ def riem_solver_c(
     with computation(FORWARD), interval(-2, -1):
         if __INLINED(A_IMP > 0.5):
             t1g = calc_t1g(dt, gam)
+            p1 = t1g / dz2 * (pem[0, 0, 1] + pp[0, 0, 1])
             if __INLINED(MOIST_CAPPA):
-                p1 = t1g * gm2 / dz2 * (pem[0, 0, 1] + pp[0, 0, 1])
-            else:
-                p1 = t1g / dz2 * (pem[0, 0, 1] + pp[0, 0, 1])
+                p1 *= gm2
             gam = aa / bet
-            bet = dm - (aa * (1 + gam) + p1)
+            bet = dm - (aa * (1.0 + gam) + p1)
             w2 = (dm * w1 + dt * (pp[0, 0, 1] - pp[0, 0, 0]
                                   ) - p1 * ws - aa * w2[0, 0, -1]) / bet
 
@@ -182,10 +177,10 @@ def riem_solver_c(
 
     # Line 332
     with computation(FORWARD):
-        with interval(0, 2):
+        with interval(0, 1):
             if __INLINED(A_IMP > 0.5):
                 pe = 0.
-        with interval(2, -1):
+        with interval(1, -1):
             if __INLINED(A_IMP > 0.5):
                 rdt = 1. / dt
                 pe = pe[0, 0, -1] + dm * (w2 - w1) * rdt
@@ -206,8 +201,11 @@ def riem_solver_c(
     # } SIM1_solver
 
     # Line 177
-    with computation(PARALLEL), interval(2, None):
+    with computation(PARALLEL), interval(1, None):
         pef = pe + pem
 
-    with computation(BACKWARD), interval(1, -2):
-        gz = gz[0, 0, 1] - dz2 * GRAV
+    with computation(BACKWARD):
+        with interval(-1, None):
+            gz = hs
+        with interval(1, -1):
+            gz = gz[0, 0, 1] - dz2 * GRAV
